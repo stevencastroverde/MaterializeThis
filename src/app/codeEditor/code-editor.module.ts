23819be477da97d83ff502bc5bce61922bcd6bcd
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2017-03-16 09:57:28 -0600

    http requests work

--- src/app/codeEditor/code-editor.module.ts
+++ src/app/codeEditor/code-editor.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
+
 import { CodeMirrorComponent } from './code-mirror/code-mirror.component';
 import { LivePreviewComponent } from './live-preview/live-preview.component';
 import { CodeEditorComponent } from './code-editor/code-editor.component';
