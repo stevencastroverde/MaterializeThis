5cdecda642e60e0636e7689a8c16456a4db054b5
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2017-03-17 10:08:43 -0600

    working

--- src/app/codeEditor/directives/editor.directive.ts
+++ src/app/codeEditor/directives/editor.directive.ts
@@ -7,6 +7,33 @@ import * as CodeMirror from 'codemirror';
 })
 export class EditorDirective {
   editor: any;
+  starterHtml: string = `<!DOCTYPE html>
+  <html>
+
+  <head>
+    <title>MaterializeThis</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+</head>
+
+<body>
+  <header>
+    //Drag your Navigation Elements HERE!!!
+
+    </header>
+<main>
+  //The content of the document......
+
+
+  </main>
+<footer>
+  //footer goes HERE!
+
+  </footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+  </body>
+
+  </html>`
 
   constructor(private renderer: Renderer) { }
 
@@ -22,35 +49,7 @@ export class EditorDirective {
       }
 
     );
-    this.editor.getDoc().setValue(`
-      <!DOCTYPE html>
-      <html>
-
-      <head>
-      <title>MaterializeThis</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
-      </head>
-
-      <body>
-        <header>
-        //Drag your Navigation Elements HERE!!!
-      
-        </header>
-        <main>
-          //The content of the document......
-      
-      
-        </main>
-        <footer>
-           //footer goes HERE!
-      
-        </footer>
-      
-       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-      </body>
-
-      </html>
-`)
+    this.editor.setValue(this.starterHtml);
   }
 
 }
