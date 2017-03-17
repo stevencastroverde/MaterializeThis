9ac3995e42caa1be7332cfa011e52ffda3294cb9
Steven <sfcastroverde@iMac.PK5001Z>
2017-03-16 20:53:30 -0600

    added basic html tags

--- src/app/codeEditor/code-mirror/code-mirror.component.ts
+++ src/app/codeEditor/code-mirror/code-mirror.component.ts
@@ -27,5 +27,4 @@ export class CodeMirrorComponent implements OnInit, OnChanges, AfterViewInit {
 
   }
 
-
 }
--- src/app/codeEditor/directives/editor.directive.ts
+++ src/app/codeEditor/directives/editor.directive.ts
@@ -15,13 +15,42 @@ export class EditorDirective {
       this.renderer.selectRootElement('[editor]'),
       {
         lineNumbers: true,
-        mode:{name:'text/html', tabMode: 'indent'},
+        mode:{name:'htmlmixed', tabMode: 'indent'},
         autofocus: true,
+        theme: 'material'
 
       }
 
     );
-    this.editor.getDoc().setValue('<html> bitch')
+    this.editor.getDoc().setValue(`
+      <!DOCTYPE html>
+      <html>
+
+      <head>
+      <title>MaterializeThis</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+      </head>
+
+      <body>
+        <header>
+        //Drag your Navigation Elements HERE!!!
+      
+        </header>
+        <main>
+          //The content of the document......
+      
+      
+        </main>
+        <footer>
+           //footer goes HERE!
+      
+        </footer>
+      
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+      </body>
+
+      </html>
+`)
   }
 
 }
